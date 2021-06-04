fb769a7e93026e985af74893ea034e0bfabcdd8c
ConnorH14 <ConnorH14@live.com>
2021-06-04 16:35:05 -0600

    fix

--- client/app/Services/PostsService.js
+++ client/app/Services/PostsService.js
@@ -6,7 +6,8 @@ class PostsService {
   async getPosts() {
     const res = await api.get('/api/posts')
     console.log(res.data, 'get')
-    ProxyState.posts = res.data
+    ProxyState.posts = res.data.map(p => new Post(p))
+    console.log(ProxyState.posts)
   }
 
   async createPost(postData) {
