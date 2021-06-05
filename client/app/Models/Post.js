58b34d8e9c63fc06b28fc3cdac7915dc1ece1064
VictorJRochaJR <vrocha897@gmail.com>
2021-06-05 09:28:09 -0600

    post

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -24,6 +24,29 @@ export class Post {
     </div>
   </div>   
  <!-- Modal  -->
+ <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel"></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p id="modalbody"></p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <form onsubmit="" id="addcomment">
+            <input type="text" placeholder="New Comment...">
+            <button type="submit">comment<button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 
     `
   }
